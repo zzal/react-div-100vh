aab7413b90bcb11622baeb7284722882af5dc079
Alain Duchesneau <alain.duchesneau@gmail.com>
2019-03-20 10:02:50 -0400

    added Props: component

--- src/lib/Div100vh.js
+++ src/lib/Div100vh.js
@@ -3,16 +3,20 @@ import convertStyle from './convertStyle';
 import getWindowHeight from './getWindowHeight';
 
 export default class Div100vh extends React.Component {
-  state = {
-    style: {}
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false,
+      style: {}
+    };
+  }
 
   // On mount and window resize converts rvh values to px (if there are any).
   // Also, adds `height: 100rvh` if height is not specified at all
   updateStyle = () => {
-    if (!this.props.once) {
+    if (!this.props.once || this.state.done) {
       const convertedStyle = convertStyle(this.props.style, getWindowHeight());
-      this.setState({ style: convertedStyle });
+      this.setState({ style: convertedStyle, done: true });
     }
   };
 
@@ -26,7 +30,12 @@ export default class Div100vh extends React.Component {
   }
 
   render() {
-    const { once, ...otherProps } = this.props;
-    return <div {...otherProps} style={this.state.style} />;
+    const { once, component, ...otherProps } = this.props;
+    let Component =
+      typeof component === 'string' && component.length > 1 ? component : 'div';
+    return React.createElement(Component, {
+      ...otherProps,
+      style: this.state.style
+    });
   }
 }
